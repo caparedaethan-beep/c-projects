33bceca478530c4fcfea4a0839d8028c5f8ad029
ethan <236763657+caparedaethan-beep@users.noreply.github.com>
2026-07-19 13:50:47 +0800

    Added a character array
    This helps to save it each character the infix expression

--- infix_to_pre_post.cpp
+++ infix_to_pre_post.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 using namespace std;
-    
-
+ 
 int main(){
 
-    string exp;
+    string exp =" ";
     int n;
+    int total;
+    char arr[total+1];
 
+    
     cout << "Input Infix Expression: ";
-    getline(cin, exp);
+    cin >> exp;
+    
+    arr[total] = '\0';
+    total = exp.length();
 
+    for(int i = 0; i < total; i++)
+        arr[i] = exp[i];
+    
     cout << "Press [1] Postfix, Press [2] Prefix: ";
     cin >> n;
     
     return 0;
+
+
 }

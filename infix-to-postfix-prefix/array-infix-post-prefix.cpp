b1354200001869736946ce66818eaf6708c9f437
ethan <236763657+caparedaethan-beep@users.noreply.github.com>
2026-07-28 23:14:50 +0800

    I added prefix standard array for converting infix to prefix.

--- infix-to-postfix-prefix/array-infix-post-prefix.cpp
+++ infix-to-postfix-prefix/array-infix-post-prefix.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <algorithm>
 
 bool isOperator(char c) {
     return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
@@ -16,21 +17,24 @@ int getPrecedence(char op) {
     return -1;
 }
 
+
 //Standard Array Postfix
 
-std::string postfix (char expression[], int total){
+std::string postfix (const std::string& infix){
     std::string postfix = ""; 
     char top = -1;
     char operatorStack[100];
-    for (int i = 0 ; i < total; i++){
-        if (isalnum(expression[i])){
-            postfix += expression[i];
+    for (char token : infix){
+        if (token == ' ') continue;
+        
+        if (isalnum(token)){
+            postfix += token;
         }
-        else if (expression[i] == '('){
+        else if (token == '('){
             top++;
-            operatorStack[top] = expression[i];
+            operatorStack[top] = token;
         }
-        else if (expression[i] == ')'){
+        else if (token == ')'){
             while (top > -1 && operatorStack[top] != '('){
                 postfix += operatorStack[top];
                 top--;
@@ -39,15 +43,15 @@ std::string postfix (char expression[], int total){
                 top--;
             }
         }
-        else if (isOperator(expression[i])){
+        else if (isOperator(token)){
             while (top > -1 && operatorStack[top] != '(' &&
-            ((!isRightAssociative(expression[i]) && getPrecedence(operatorStack[top]) >= getPrecedence(expression[i])) ||
-            (isRightAssociative(expression[i]) && getPrecedence(operatorStack[top]) > getPrecedence(expression[i])))){
+            ((!isRightAssociative(token) && getPrecedence(operatorStack[top]) >= getPrecedence(token)) ||
+            (isRightAssociative(token) && getPrecedence(operatorStack[top]) > getPrecedence(token)))){
                 postfix += operatorStack[top];
                 top--;
             }
             top++;
-            operatorStack[top] = expression[i];
+            operatorStack[top] = token;
         }
     }
     while (top > -1){
@@ -57,47 +61,80 @@ std::string postfix (char expression[], int total){
     return postfix;
 }
 
+//Standard Array Prefix
+
+std::string prefix (const std::string& infix){
+    std::string reversedResult = ""; 
+    int top = -1;
+    char operatorStack[100];
+    std::string reversedInfix = infix;
+    std::reverse(reversedInfix.begin(), reversedInfix.end());
+    for (char token : infix){
+        if (token == ' ') continue;
+        
+        if (isalnum(token)){
+            reversedResult += token;
+        }
+        else if (token == ')'){
+            top++;
+            operatorStack[top] = token;
+        }
+        else if (token == '('){
+            while (top > -1 && operatorStack[top] != ')'){
+                reversedResult += operatorStack[top];
+                top--;
+            }
+            if (top > -1) {
+                top--;
+            }
+        }
+        else if (isOperator(token)){
+            while (top > -1 && operatorStack[top] != '(' &&
+            ((!isRightAssociative(token) && getPrecedence(operatorStack[top]) >= getPrecedence(token)) ||
+            (isRightAssociative(token) && getPrecedence(operatorStack[top]) > getPrecedence(token)))){
+                reversedResult += operatorStack[top];
+                top--;
+            }
+            top++;
+            operatorStack[top] = token;
+        }
+    }
+    while (top > -1){
+        reversedResult += operatorStack[top];
+        top --;
+    }
+    std::string prefix = reversedResult;
+    std::reverse(prefix.begin(), prefix.end());
+    return prefix;
+}
+
+
+
+
+
 int main(){
 
     int n; 
-    std::string convertExpression = "";
+    std::string expression = "";
     int total = 0;
     std::cout << "Input Infix Notation: ";
-    std::getline(std::cin, convertExpression);
+    std::getline(std::cin, expression);
     std::cout <<"What conversion do you want? [1] Prefix Notation, [2] Postfix Notation";
     std::cin >> n;
 
-    char expression[total+1] = {'\0'};
-
-    for (int i = 0; i <convertExpression.length(); i++){
-        if (expression[i] != ' '){
-            expression[total] = convertExpression[i];
-            total++;
-        }
+    if (n == 1){
+    std::cout << "\n" << std::string(50, '=') << "\n";
+    std::cout << "FINAL RESULTS:\n";
+    std::cout << "Infix:   " << expression << "\n";
+    std::cout << "Prefix:  " << prefix(expression) << "\n";
     }
 
-    std::cout << postfix(expression, total);
-
-    // if (n == 1){
-    // // std:: string prefix =
-    // std::cout << "\n" << std::string(50, '=') << "\n";
-    // std::cout << "FINAL RESULTS:\n";
-    // std::cout << "Infix:   " << expression << "\n";
-    // std::cout << "Prefix:  " << prefix << "\n";
-    // }
-
-    // else if (n == 2){
-
-    // }
-    // // std::string postfix = 
-    // std::cout << "\n" << std::string(50, '=') << "\n";
-    // std::cout << "FINAL RESULTS:\n";
-    // std::cout << "Infix:   " << expression << "\n";
-    // std::cout << "Postfix:  " << postfix << "\n";
-    // }
-    
-
-
+    else if (n == 2){  
+    std::cout << "\n" << std::string(50, '=') << "\n";
+    std::cout << "FINAL RESULTS:\n";
+    std::cout << "Infix:   " << expression << "\n";
+    std::cout << "Postfix:  " << postfix(expression)<< "\n";
+    }
 
 
     

134a70778851c84a5e2179577535ccae1649aa3c
ethan <236763657+caparedaethan-beep@users.noreply.github.com>
2026-08-13 20:49:26 +0800

    Added laboratory exercise 4 practice c++ code.

---
+++ Laboratory-Exercise-4/laboratory-exercise-4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main(){
+
+    int value1, value2;
+    std::cout << "Input Integer 1: ";
+    std::cin >> value1;
+    std::cout << "\nInput Integer 2: ";
+    std::cin >> value2;
+
+    
+    return 0;
+}

07b8b12529f6d8f52b93dda948bf2a445efa76fe
ethan <236763657+caparedaethan-beep@users.noreply.github.com>
2026-09-04 22:54:02 +0800

    I added the node structure and the output of the code.
    This will help as my basis in creating node and how it will go out with my output.

--- Laboratory-Exercise-5/laboratoryexercise#5.cpp
+++ Laboratory-Exercise-5/laboratoryexercise#5.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
+#include <cctype>
+
+//This is where we will use to add node
+struct node {
+    char data;
+    node* left;
+    node* right;
+
+    //Constructor
+    node (char value){
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+// void insertNode (node*& node, char value){
+//     if (node == nullptr) return;
+// }
+
+
+//void inOrder
+
+//void preOrder
+
+//void postOrder
 
 int main(){
-    int n[15];
-
+    node* root = nullptr;
+    char node[15];
+    //This will input data to use for our node
     for (int i = 0; i < 15; i++){
-        std::cin >> n[i];
+        std::cin >> node[i];
+        node[i] = std::toupper(node[i]); //This will make it uppercase
+        // insertNode (root, node[i]);
     }
 
-    for (int i = 0; i < 15; i++){
-        std::cout << n[i];
-    }
+
+    std::cout << "Pre-order Traversal: ";
+    // preOrder(root);
+    std::cout << std::endl;
+    std::cout << "In-order Traversal: ";
+    //inOrder(root);
+    std::cout << std::endl;
+    std::cout << "Post-order Traversal: ";
+    //postOrder(root);
+    std::cout << std::endl;
+    
 }
